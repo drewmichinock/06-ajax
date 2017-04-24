16614d7073d879274ae7beba0420bab08b443410
drewmichinock <drewmichinock@gmail.com>
2017-04-24 16:16:21 -0700

    2nd commit

---
+++ assets/javascript/ajax.js
@@ -0,0 +1,4 @@
+var = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird",
+    "ferret", "turtle", "sugar glider", "chinchilla", "hedgehog", "hermit crab", "gerbil",
+    "pygmy goat", "chicken", "capybara", "teacup pig", "serval", "salamander", "frog"
+];
