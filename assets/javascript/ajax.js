672fa55d741bf3ce3c49962f850c86b494474ec2
drewmichinock <drewmichinock@gmail.com>
2017-04-25 00:00:04 -0700

    3rd commit

--- assets/javascript/ajax.js
+++ assets/javascript/ajax.js
@@ -1,4 +1,24 @@
-var = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird",
-    "ferret", "turtle", "sugar glider", "chinchilla", "hedgehog", "hermit crab", "gerbil",
-    "pygmy goat", "chicken", "capybara", "teacup pig", "serval", "salamander", "frog"
-];
+$(document).ready(function() {
+
+    // default animals
+    var animals = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird",
+        "ferret", "turtle", "sugar glider", "chinchilla", "hedgehog", "hermit crab", "gerbil",
+        "pygmy goat", "chicken", "capybara", "teacup pig", "serval", "salamander", "frog"
+    ];
+
+    // create default animals buttons
+    for (var i = 0; i < animals.length; i++) {
+
+        var animalBtn = $("<button>");
+
+        animalBtn.addClass("animal-btn");
+
+        animalBtn.attr("data-animal", animals[i]);
+
+        animalBtn.text(animals[i]);
+
+        $("#animal-buttons").append(animalBtn);
+
+    }
+
+});
