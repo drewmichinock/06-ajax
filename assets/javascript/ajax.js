bb2cb7811fd65e007acd181855ea781ef96ca3ea
drewmichinock <drewmichinock@gmail.com>
2017-04-25 20:55:27 -0700

    13th commit

--- assets/javascript/ajax.js
+++ assets/javascript/ajax.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
 
     // default animal button labels
-    var animals = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird",
-        "ferret", "turtle", "sugar glider", "chinchilla", "hedgehog", "hermit crab", "gerbil",
-        "pygmy goat", "chicken", "capybara", "teacup pig", "guinea pig", "lizard", "frog"
+    var animals = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish",
+        "bird", "ferret", "turtle", "sugar glider", "chinchilla", "hedgehog",
+        "hermit crab", "gerbil", "pygmy goat", "chicken", "capybara",
+        "teacup pig", "guinea pig", "lizard", "frog"
     ];
 
     // creating buttons from default animal button labels array
@@ -94,29 +95,55 @@ $(document).ready(function() {
 
                 // ... assign the following attributes and values
                 animalImg.attr("src", results[i].images.fixed_height_still.url);
+
                 animalImg.attr("data-still", results[i].images.fixed_height_still.url);
+
                 animalImg.attr("data-animate", results[i].images.fixed_height.url);
+
                 animalImg.attr("data-state", "still");
+
                 animalImg.attr("class", "gif");
 
                 // ... place p and img tag inside div
                 animalDiv.append(p);
+
                 animalDiv.append(animalImg);
 
                 // ... and insert them at beginning of #animal-img div tag
                 $("#animal-img").prepend(animalDiv);
 
             }
+
+            // when gif is clicked
+            $(".gif").on("click", function() {
+
+                // create variable for if/else statement
+                var state = $(this).attr("data-state");
+
+                // if data-state value is still
+                if (state === "still") {
+
+                    // change src value to data-animate value ...
+                    $(this).attr("src", $(this).attr("data-animate"));
+
+                    // change data-state value to animate
+                    $(this).attr("data-state", "animate");
+
+                    // if data-state value is animate
+                } else {
+
+                    // change src value to data-still value
+                    $(this).attr("src", $(this).attr("data-still"));
+
+                    // change data-state value to still
+                    $(this).attr("data-state", "still");
+
+                }
+
+            });
+
         })
 
     });
-
-    // when gif is clicked
-    $(".gif").on("click", function() {
-
-    	// 
-    	var state = $(this)
-
-    })
 
 });
